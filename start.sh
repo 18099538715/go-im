c63ebe4318aae7692b3de12c4276ff4da44d01a6
liupengfei <18099538715@163.com>
2018-05-08 13:02:49 +0800

    modify start.sh

--- start.sh
+++ start.sh
@@ -5,17 +5,17 @@ export GOPATH=$baseDir/api
 cd $baseDir/api/src
 go get 
 go build apimain.go
-nohup ./apimain & >/dev/null 2>&1 
+nohup ./apimain  >/dev/null 2>&1 &
 echo "api start success"
 export GOPATH=$baseDir/connect
 cd $baseDir/connect/src
 go get
 go build connectmain.go
-nohup ./connectmain & >/dev/null 2>&1 
+nohup ./connectmain  >/dev/null 2>&1  &
 echo "connect start success"
 export GOPATH=$baseDir/logic
 cd $baseDir/logic/src
 go get
 go build logicmain.go
-nohup ./logicmain & >/dev/null 2>&1 
+nohup ./logicmain  >/dev/null 2>&1   &
 echo "logic start success"
